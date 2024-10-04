f881d62b6ee7ede559b18d0447abd4d20d0406a7
Jayme Klein <jaymerebula04@gmail.com>
2024-10-03 21:45:15 -0300

    feat(Matrix): :sparkles: Matrix multiplication

---
+++ matrix_multiplication/matrix_multiplication.py
@@ -0,0 +1,99 @@
+import multiprocessing
+from tqdm import tqdm  # Biblioteca para estimar tempo de iterações de um loop.
+
+
+def load_matrix(filename: str) -> list[list[float]]:
+    with open(filename, "r") as f:
+        matrix = []
+
+        for line in f:
+            row = list(map(float, line.strip().split()))
+            matrix.append(row)
+
+    return matrix
+
+
+def multiply_row_by_matrix(row: list[float], matB: list[list[float]]) -> list[float]:
+    result_row = []
+    for j in range(len(matB[0])):
+        element_sum = 0
+
+        for k in range(len(row)):
+            element_sum += row[k] * matB[k][j]
+
+        result_row.append(element_sum)
+
+    return result_row
+
+
+def matrix_multiply_worker(
+    matA: list[list[float]],
+    matB: list[list[float]],
+    rows_range: tuple[int, int],
+    output: list[list[float]],
+    index: int,
+):
+    partial_result = []
+    for i in tqdm(range(*rows_range), desc=f"Processador {index}"):
+        partial_result.append(multiply_row_by_matrix(matA[i], matB))
+
+    output[index] = partial_result
+
+
+def parallel_matrix_multiply(
+    matA: list[list[float]], matB: list[list[float]], num_workers: int
+):
+    print("Multiplicando matrizes.")
+    manager = multiprocessing.Manager()
+    output = manager.dict()
+
+    rows_per_worker = len(matA) // num_workers
+    processes = []
+
+    for i in range(num_workers):
+        start_row = i * rows_per_worker
+        if i == num_workers - 1:
+            end_row = len(matA)
+
+        else:
+            end_row = (i + 1) * rows_per_worker
+
+        process = multiprocessing.Process(
+            target=matrix_multiply_worker,
+            args=(matA, matB, (start_row, end_row), output, i),
+        )
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    result = []
+    for i in range(num_workers):
+        result.extend(output[i])
+
+    return result
+
+
+def print_matrix(matrix: list[list[float]]):
+    for row in matrix:
+        print("\t".join(map(str, row)))
+
+
+if __name__ == "__main__":
+    matA = load_matrix(r"C:/Users/jayme/Downloads/matA_B/matA.txt")
+    matB = load_matrix(r"C:/Users/jayme/Downloads/matA_B/matB.txt")
+
+    if len(matA[0]) != len(matB):
+        raise ValueError(
+            "As matrizes não podem ser multiplicadas. O número de colunas de A deve ser igual ao número de linhas de B."
+        )
+
+    # num_workers = multiprocessing.cpu_count()  # Aumente por sua conta e risco.
+    num_workers: int = 4
+    print(f"Multiplicando matrizes usando {num_workers} processadores lógicos.")
+
+    result = parallel_matrix_multiply(matA, matB, num_workers)
+
+    print("Resultado da multiplicação de matrizes:")
+    print_matrix(result)
